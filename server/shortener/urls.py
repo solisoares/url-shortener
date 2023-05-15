98d9273c40228e64b4d7adbfa2f5295845952796
solisoares <alexandresoares@onsign.tv>
2023-05-15 09:57:44 -0300

    Create shortener index view
    The index view renders the index.html template that shows a form to
    shorten a URL.

---
+++ server/shortener/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = "shortener"  # app namespace
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("shorten/", views.shorten, name="shorten"),
+]
--- server/shortener/views.py
+++ server/shortener/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request: HttpRequest):
+    context = {"short_url": None}
+    return render(request, "shortener/index.html", context)
+
+
+def shorten(request: HttpRequest):
+    # TODO
+    return HttpResponse("shorten-response")
