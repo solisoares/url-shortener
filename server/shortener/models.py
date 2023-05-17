05e8cb7418630d1b46b6c88313025fa80e1e3aff
solisoares <alexandresoares@onsign.tv>
2023-05-17 17:26:23 -0300

    Override URL's save method with short url creation
    Changes:
    - When the URL model is created, it first save a version with only the
    original url, then it calls an auxiliary method to create the shortened
    url, then it saves again.

--- server/shortener/models.py
+++ server/shortener/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
+import hashids
 
 # Create your models here.
 class URL(models.Model):
     original_url = models.URLField(unique=True, max_length=200)
-    short_url = models.CharField(unique=True, max_length=5)
+    short_url = models.CharField(unique=True, max_length=5, blank=True)
     creation_datetime = models.DateTimeField(auto_now_add=True)
     last_access_datetime = models.DateTimeField(auto_now=True)
     access_count = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        """Overrides save by automatically creating and saving the short url"""
+        is_new = not self.pk  # self.pk is initialized after DB insertion
+        super().save(*args, **kwargs)  # regular saving process
+
+        if is_new:
+            self.short_url = self._hash_url()
+            super().save(update_fields=["short_url"])
+
+    def _hash_url(self):
+        hashid = hashids.Hashids(salt="42", min_length=5)
+        return hashid.encode(self.pk)
--- server/shortener/views.py
+++ server/shortener/views.py
@@ -2,30 +2,29 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 from .forms import ShortenerForm
+from .models import URL
 
 
 # Create your views here.
 def index(request: HttpRequest):
     if request.method == "POST":
-        # TODO
-        # - colect original url
-        # - generate shortened url
-        # - save to database
-
         shortener_form = ShortenerForm(request.POST)
 
-        # STUB -------------------------
-        shortened_url = "SHORTENED URL"
-        # ------------------------------
-
-        return render(
-            request,
-            "shortener/index.html",
-            {"shortener_form": shortener_form, "shortened_url": shortened_url},
-        )
+        if shortener_form.is_valid():
+            original_url: str = shortener_form.cleaned_data["original_url"]
+            url_model = URL(original_url=original_url)
+            url_model.save()
+            return render(
+                request,
+                "shortener/index.html",
+                {
+                    "shortener_form": shortener_form,
+                    "shortened_url": f"http://{request.get_host()}/"
+                    + url_model.short_url,
+                },
+            )
 
     else:
         shortener_form = ShortenerForm()
-        return render(
-            request, "shortener/index.html", {"shortener_form": shortener_form}
-        )
+
+    return render(request, "shortener/index.html", {"shortener_form": shortener_form})
