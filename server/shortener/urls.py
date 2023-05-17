b9d02bcf17820fd5dc2f2a6b214dd448e14699ea
solisoares <alexandresoares@onsign.tv>
2023-05-17 14:34:51 -0300

    Show form and result in the index view
    Context:
    - Commit e2a1f15 (Index page shows only the form, not the result)
    introduced a view to show the result in a different template.
    - But it is nice to have the result show up at the same template the
    form was submitted.
    
    Changes:
    - Add the result shortened url at the shortener/index.html template
    - Remove, now unnecessary, urls and templates.
    - Update the index view with stubs for POST and GET

--- server/shortener/urls.py
+++ server/shortener/urls.py
@@ -7,6 +7,4 @@ app_name = "shortener"  # app namespace
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("shorten/", views.shorten, name="shorten"),
-    path("shortened/<shortened_url>", views.shortened, name="shortened"),
 ]
--- server/shortener/views.py
+++ server/shortener/views.py
@@ -1,33 +1,31 @@
-from typing import Union
-
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest
 from django.shortcuts import render
-from django.urls import reverse
+
+from .forms import ShortenerForm
 
 
 # Create your views here.
-def index(request: HttpRequest, short_url: Union[str, None] = None):
-    return render(request, "shortener/index.html")
+def index(request: HttpRequest):
+    if request.method == "POST":
+        # TODO
+        # - colect original url
+        # - generate shortened url
+        # - save to database
 
+        shortener_form = ShortenerForm(request.POST)
 
-def shorten(request: HttpRequest):
-    # TODO
-    # - colect original url
-    # - generate shortened url
-    # - save to database
-    # - redirect to shortened view with shortened url as arg
+        # STUB -------------------------
+        shortened_url = "SHORTENED URL"
+        # ------------------------------
 
-    # STUB
-    shortened_url = "SHORTENED URL"
-    return HttpResponseRedirect(reverse("shortener:shortened", args=(shortened_url,)))
+        return render(
+            request,
+            "shortener/index.html",
+            {"shortener_form": shortener_form, "shortened_url": shortened_url},
+        )
 
-
-def shortened(request: HttpRequest, shortened_url: Union[str, None]):
-    # TODO
-    # - get shortened and original url from database
-    # - render the shortened page with original and shortened url as context
-
-    # STUB
-    original_url = "ORIGINAL URL"
-    context = {"original_url": original_url, "shortened_url": shortened_url}
-    return render(request, "shortener/shortened.html", context)
+    else:
+        shortener_form = ShortenerForm()
+        return render(
+            request, "shortener/index.html", {"shortener_form": shortener_form}
+        )
