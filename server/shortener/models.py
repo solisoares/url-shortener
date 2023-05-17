d6dc2729d87a648edecc1434a8c9726774c102ba
solisoares <alexandresoares@onsign.tv>
2023-05-17 09:21:35 -0300

    Add URL model

--- server/shortener/models.py
+++ server/shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class URL(models.Model):
+    original_url = models.URLField(unique=True, max_length=200)
+    short_url = models.CharField(unique=True, max_length=5)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    last_access_datetime = models.DateTimeField(auto_now=True)
+    access_count = models.IntegerField(default=0)
